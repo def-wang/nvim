1a4be457688db7706e55d6ab20015f3c658deecd
wangdefu <wangdefu@zhihu.com>
2024-04-17 17:42:09 +0800

    init vim config

---
+++ lua/chadrc.lua
@@ -0,0 +1,19 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+	theme = "doomchad",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+	nvdash = {
+		load_on_startup = true,
+	},
+}
+
+return M
---
+++ lua/plugins/init.lua
@@ -0,0 +1,59 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+   {
+     "neovim/nvim-lspconfig",
+     config = function()
+       require("nvchad.configs.lspconfig").defaults()
+       require "configs.lspconfig"
+     end,
+   },
+  --
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua"
+      },
+    },
+  },
+  --
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+        "vimdoc",
+
+        -- web dev
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+
+        --other
+        "json",
+        "python",
+        "yaml",
+        "java",
+        "markdown",
+        -- low level
+        "c",
+        "zig"
+      },
+    },
+  },
+}
